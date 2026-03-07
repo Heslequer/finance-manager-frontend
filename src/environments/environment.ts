f67a2e345d7cf5e459f1079dee136a8347ba8230
Heslequer <hesleiquer5@gmail.com>
2026-03-07 10:49:57 -0300

    chore: update backend API URL to Cloud Run endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  backendApiUrl: 'https://finance-manager-backend-smoky.vercel.app',
+  backendApiUrl: 'https://finance-manager-backend-928640916839.southamerica-east1.run.app',
 } as const;
